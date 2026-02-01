e5281d592ca794fcaef40044f68d5ac6c0e62945
Pratham Jain <jainpratham1410@gmail.com>
2026-02-01 16:08:48 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.in",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
